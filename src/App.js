c5057c96b04f88a323c4da8c7cf55c2026e808ab
Ascheritt <52357621+Ascheritt@users.noreply.github.com>
2021-03-19 23:05:44 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Copeteeeee
+          Learn Copeteeeee in apex!!!
         </a>
       </header>
     </div>
